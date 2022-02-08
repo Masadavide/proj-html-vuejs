4c8d8ab6aef83e39ec6b220d17cb38946ee41dc8
Masadavide <davide.masadm@gmail.com>
2022-02-08 11:31:28 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+      <div class="container">
+        Header
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+      <div class="container">
+        Main
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
